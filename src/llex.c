d1c9ddf047cf0b4a2f2288c25de5e9a6d004ff8d
Kai D. Gonzalez <gkai70264@gmail.com>
2021-08-28 13:43:47 -0400

    change '~=' to '!='

--- src/llex.c
+++ src/llex.c
@@ -505,10 +505,10 @@ static int llex (LexState *ls, SemInfo *seminfo) {
         if (check_next1(ls, '/')) return TK_IDIV;  /* '//' */
         else return '/';
       }
-      case '~': {
+      case '!': {
         next(ls);
         if (check_next1(ls, '=')) return TK_NE;  /* '~=' */
-        else return '~';
+        else return '!';
       }
       case ':': {
         next(ls);
